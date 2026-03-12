52c3898226066e6fba02500e6a341334cd0c78f8
nickchinchila <nicolli50@outlook.com>
2026-03-13 04:39:16 +0800

    Chi-square analysis library for Hide & Seek data

---
+++ tests_and_examples/.ipynb_checkpoints/teste_single_node-checkpoint.py
@@ -0,0 +1,40 @@
+"""
+How to use Chi2_analysis_for_H&S_data library with an example
+
+[10/03/2026] - Nicolli Soares
+"""
+# =================================================================================================
+# Import from the library the submission coordinator
+from Chi2_analysis_for_HideSeek_data import run_parallel
+
+#--------------------------------------------------------------------------------------------------
+# Your code here (can be a serial code or parallel code)
+#--------------------------------------------------------------------------------------------------
+
+# Set the parameters of your analysis
+analysis_parameters = {
+	"n_horns": 140,
+	"n_hours": 24,
+	"n_bins": 30, 
+	"obs_date": "20200301",                                  # data format: yyyymmdd
+	"base_results_path": "/data/NSOARES/Chi2_results/",
+	"base_obsTOD_path": "/data/NSOARES/hide_seek/output_test_parallel_140/2020/03/01/",
+	"base_expTOD_path": "/data/NSOARES/hide_seek/output_test_for_chi2_lib/2020/03/01/",
+	"dof": None,                                             # optional parameter with standard value "None"
+	"analysis_identifier":,                             # optional parameter with standard value as a random number
+	"show_process_info": False,                               # optional parameter with standard value "False"
+	"rmse": True,                                           # optional parameter with standard value "False"
+	"plot_waterfalls": True,                                 # optional parameter with standard value "False"
+	"horns_to_plot": None,                                    # optional parameter with standard value "None"
+	"min_valid_samples": 10
+}
+
+# Run Chi2_analysis_for_H&S_data library
+run_parallel(analysis_parameters,                         
+				 num_nodes=1,                                # number of nodes you want to use
+				 total_num_process=48,
+				 slurm=False)                     # total number of processes you want to run in parallel
+
+#--------------------------------------------------------------------------------------------------
+# Your code here (can be a serial code or parallel code)
+#--------------------------------------------------------------------------------------------------
